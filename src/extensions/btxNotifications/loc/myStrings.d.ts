833b31e796399f2ad99843870b50e1db45b23b53
Jignesh Maru <jmaru@tech.saisystems.com>
2026-02-09 12:44:02 +0530

    commit

---
+++ src/extensions/btxNotifications/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IBtxNotificationsApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'BtxNotificationsApplicationCustomizerStrings' {
+  const strings: IBtxNotificationsApplicationCustomizerStrings;
+  export = strings;
+}
