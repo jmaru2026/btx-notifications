ae597d2a81e951648e63e344121e257619350ca9
Jignesh Maru <jmaru@tech.saisystems.com>
2026-02-11 12:27:22 +0530

    made some deisgn changes

--- src/extensions/btxNotifications/BtxNotificationsApplicationCustomizer.ts
+++ src/extensions/btxNotifications/BtxNotificationsApplicationCustomizer.ts
@@ -171,7 +171,39 @@ private async _checkNew() {
   this._updateBadge();
 }
 
+private _formatDate(dateString: string): string {
 
+  const created = new Date(dateString);
+  const now = new Date();
+
+  const diffMs = now.getTime() - created.getTime();
+
+  const sec = Math.floor(diffMs / 1000);
+  const min = Math.floor(sec / 60);
+  const hr = Math.floor(min / 60);
+  const day = Math.floor(hr / 24);
+
+  if (sec < 60) return 'Just now';
+
+  if (min < 60)
+    return `${min} minute${min > 1 ? 's' : ''} ago`;
+
+  if (hr < 24)
+    return `${hr} hour${hr > 1 ? 's' : ''} ago`;
+
+  if (day === 1)
+    return 'Yesterday';
+
+  if (day < 7)
+    return `${day} days ago`;
+
+  // older → show date like SharePoint
+  return created.toLocaleDateString('en-GB', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric'
+  });
+}
 
   /* ---------------------------------------
      PANEL RENDER
@@ -208,6 +240,7 @@ private async _checkNew() {
       row.innerHTML = `
         <div class="btxTitle">${n.Title}</div>
         <div class="btxDesc">${n.Description || ''}</div>
+        <div class="CreatedDate">${this._formatDate(n?.Created) || ''}</div>
       `;
 
       if (n?.Link)
